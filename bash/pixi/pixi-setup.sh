5712e0b27944a2f9499f1a7a7a40b24c7fbd7a9c
gaow <wangow@gmail.com>
2024-05-30 09:25:00 -0400

    Update pixi-setup.sh

--- bash/pixi/pixi-setup.sh
+++ bash/pixi/pixi-setup.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # Install pixi
-curl -fsSL https://pixi.sh/install.sh | sed '/set -euo pipefail/c\set -e' | bash
+curl -fsSL https://pixi.sh/install.sh | bash
 
 # Configure shell
 if ! grep -q 'export PATH="${HOME}/.pixi/bin:${PATH}"' "${CONFIG_FILE}"; then
