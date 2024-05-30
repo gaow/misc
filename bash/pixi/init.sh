c54156396b8aefe9b73e397c272c66fc43211a14
gaow <wangow@gmail.com>
2024-05-30 08:56:07 -0400

    Update init.sh

--- bash/pixi/init.sh
+++ bash/pixi/init.sh
@@ -3,12 +3,25 @@
 set -o errexit -o xtrace
 
 # Only show PYTHONPATH and R_LIBS to specific executables
-sed -i '2i export PYTHONPATH="${HOME}/micromamba/envs/python_libs/lib/python3.12/site-packages"' ${HOME}/.pixi/bin/python
-sed -i '2i export PYTHONPATH="${HOME}/micromamba/envs/python_libs/lib/python3.12/site-packages"' ${HOME}/.pixi/bin/python3
-sed -i '2i export PYTHONPATH="${HOME}/micromamba/envs/python_libs/lib/python3.12/site-packages"' ${HOME}/.pixi/bin/jupyter-lab
-sed -i '2i export PYTHONPATH="${HOME}/micromamba/envs/python_libs/lib/python3.12/site-packages"' ${HOME}/.pixi/bin/jupyter-server
-sed -i '2i export PYTHONPATH="${HOME}/micromamba/envs/python_libs/lib/python3.12/site-packages"' ${HOME}/.pixi/bin/sos
-sed -i "1i .libPaths(\"${HOME}/micromamba/envs/r_libs/lib/R/library\")" ${HOME}/.Rprofile
+export PIXI_HOME="${HOME}/.pixi"
+export MAMBA_ROOT_PREFIX="${HOME}/micromamba"
+
+tee ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py << EOF
+import sys
+sys.path[0:0] = [
+    "${MAMBA_ROOT_PREFIX}/envs/python_libs/lib/python3.12/site-packages"
+]
+EOF
+
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/jupyter_client/lib/python3.12/site-packages/
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/jupyter_console/lib/python3.12/site-packages/
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/jupyter_core/lib/python3.12/site-packages/
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/jupyter_server/lib/python3.12/site-packages/
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/jupyterlab/lib/python3.12/site-packages/
+ln -f ${PIXI_HOME}/envs/python/lib/python3.12/site-packages/sitecustomize.py ${PIXI_HOME}/envs/sos/lib/python3.12/site-packages/
+
+mkdir -p ${PIXI_HOME}/envs/r-base/lib/R/etc
+echo ".libPaths('${MAMBA_ROOT_PREFIX}/envs/r_libs/lib/R/library')" >> ${PIXI_HOME}/envs/r-base/lib/R/etc/Rprofile.site
 
 # pixi global currently gives it wrappers all lowercase names, so we need to make symlinks for R and Rscript
 if [ ! -e "${HOME}/.pixi/bin/R" ]; then
