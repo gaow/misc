a92ca490992a870f4c2022c34e42ada5a12265a2
gaow <wangow@gmail.com>
2024-06-15 10:17:37 -0400

    Update pixi-setup.sh

--- bash/pixi/pixi-setup.sh
+++ bash/pixi/pixi-setup.sh
@@ -1,54 +1,8 @@
 set -euo pipefail
-# Determine the current shell
-if [ -v ZSH_VERSION ]; then
-  # Zsh shell
-  CONFIG_FILE="${HOME}/.zshrc"
-elif [ -v BASH_VERSION ]; then
-  # Bash shell
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
-    CONFIG_FILE="${HOME}/.bashrc"
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    CONFIG_FILE="${HOME}/.bash_profile"
-  fi
-else
-  echo "Unsupported shell. Please use bash or zsh."
-  exit 1
-fi
-
-echo "Configuration file ${CONFIG_FILE} will be modified by this script."
-
 # Install pixi
-if ! command -v pixi &> /dev/null
-then
-    # Install Pixi
-    curl -fsSL https://pixi.sh/install.sh | bash
-        
-    if ! grep -q 'export PATH="${HOME}/.pixi/bin:${PATH}"' "${CONFIG_FILE}"; then
-        echo 'export PATH="${HOME}/.pixi/bin:${PATH}"' >> "${CONFIG_FILE}"
-        export PATH="${HOME}/.pixi/bin:${PATH}"
-    fi
-else
-    echo "Pixi is already installed."
-fi
+curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/pixi-config.sh | bash 
 
-# Configure shell
-
-if ! grep -q 'unset PYTHONPATH' "${CONFIG_FILE}"; then
-  echo "unset PYTHONPATH" >> "${CONFIG_FILE}"
-  unset PYTHONPATH
-fi
-
-if ! grep -q 'export PYDEVD_DISABLE_FILE_VALIDATION=1' "${CONFIG_FILE}"; then
-  echo "export PYDEVD_DISABLE_FILE_VALIDATION=1" >> "${CONFIG_FILE}"
-  export PYDEVD_DISABLE_FILE_VALIDATION=1
-fi
-
-# set default channels
-mkdir -p ${HOME}/.config/pixi && echo 'default_channels = ["dnachun", "conda-forge", "bioconda"]' > ${HOME}/.config/pixi/config.toml
-
-# install global packages
+# Install global packages
 pixi global install $(curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/global_packages.txt | tr '\n' ' ')
 
 # install R and Python libraries currently via micromamba although later pixi will also support installing them in `global` as libraries without `bin`
