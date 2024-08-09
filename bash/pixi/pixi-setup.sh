eb816105f399271f908c30b51d43564c3ed5f5b9
gaow <wangow@gmail.com>
2024-08-08 20:56:21 -0400

    Update pixi-setup.sh

--- bash/pixi/pixi-setup.sh
+++ bash/pixi/pixi-setup.sh
@@ -12,35 +12,36 @@ else
 fi
 
 echo "Configuration file ${CONFIG_FILE} will be modified by this script."
+touch ${CONFIG_FILE}
 
 # Install pixi
 if ! command -v pixi &> /dev/null
 then
     # Install Pixi
     curl -fsSL https://pixi.sh/install.sh | bash
-
-    if ! grep -q 'export PATH="${HOME}/.pixi/bin:${PATH}"' "${CONFIG_FILE}"; then
-        echo 'export PATH="${HOME}/.pixi/bin:${PATH}"' >> "${CONFIG_FILE}"
-        export PATH="${HOME}/.pixi/bin:${PATH}"
-    fi
-    BB='\033[1;34m'
-    NC='\033[0m'
-    echo -e "${BB}Pixi installed. Please run 'source ${CONFIG_FILE}' to reload your configuration or restart your terminal, and rerun this setup script.${NC}"
-    exit 1
 else
     echo "Pixi is already installed."
 fi
 
 # Configure shell
+if ! grep -q 'export PATH="${HOME}/.pixi/bin:${PATH}"' "${CONFIG_FILE}"; then
+    echo 'export PATH="${HOME}/.pixi/bin:${PATH}"' >> "${CONFIG_FILE}"
+    export PATH="${HOME}/.pixi/bin:${PATH}"
+fi
 if ! grep -q 'unset PYTHONPATH' "${CONFIG_FILE}"; then
   echo "unset PYTHONPATH" >> "${CONFIG_FILE}"
   unset PYTHONPATH
 fi
-
 if ! grep -q 'export PYDEVD_DISABLE_FILE_VALIDATION=1' "${CONFIG_FILE}"; then
   echo "export PYDEVD_DISABLE_FILE_VALIDATION=1" >> "${CONFIG_FILE}"
   export PYDEVD_DISABLE_FILE_VALIDATION=1
 fi
-
+if ! command -v pixi &> /dev/null
+then
+    BB='\033[1;34m'
+    NC='\033[0m'
+    echo -e "${BB}Pixi installed. Please run 'source ${CONFIG_FILE}' to reload your configuration or restart your terminal, and rerun this setup script.${NC}"
+    exit 1
+fi
 # set default channels
 mkdir -p ${HOME}/.config/pixi && echo 'default_channels = ["dnachun", "conda-forge", "bioconda"]' > ${HOME}/.config/pixi/config.toml
