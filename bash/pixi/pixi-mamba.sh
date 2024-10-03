057c2df33fffb123119551862888c026df1dd3fd
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-03 12:24:38 -0700

    fix micromamba

--- bash/pixi/pixi-mamba.sh
+++ bash/pixi/pixi-mamba.sh
@@ -7,18 +7,18 @@ export MAMBA_ROOT_PREFIX="${HOME}/micromamba"
 curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/pixi-setup.sh | bash
 
 # Install global packages
-curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/global_packages.txt | xargs -I % pixi global install %
+curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/global_packages.txt | xargs -I % pixi global install --quiet %
 
 # install R and Python libraries currently via micromamba although later pixi will also support installing them in `global` as libraries without `bin`
 # NOTE: This is assuming a first-time run
 if [ -n ${HOME}/micromamba/envs/etc ]; then
     rm -rf ${HOME}/micromamba/etc
 fi
-micromamba config prepend channels nodefaults 
+micromamba config prepend channels nodefaults
 micromamba config prepend channels bioconda
 micromamba config prepend channels conda-forge
 micromamba config prepend channels dnachun
-micromamba shell init --shell=bash ${HOME}/micromamba
+micromamba shell init --quiet --shell=bash ${HOME}/micromamba
 
 if [ -n ${HOME}/micromamba/envs/etc ]; then
     rm -rf ${HOME}/micromamba/etc
@@ -27,9 +27,9 @@ fi
 rm -rf ${HOME}/.mambarc
 echo "use_lockfiles: false" >> ${HOME}/.mambarc
 
-curl -O https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/r.yml && micromamba env create --yes -q --file r.yml && rm -f r.yml
-curl -O https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/python.yml && micromamba env create --yes -q --file python.yml && rm -f python.yml
-micromamba clean --all --yes
+curl -O https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/r.yml && micromamba env create --yes --quiet --file r.yml && rm -f r.yml
+curl -O https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/python.yml && micromamba env create --yes --quiet --file python.yml && rm -f python.yml
+micromamba clean --all --yes --quiet
 
 # fix R and Python settings 
 curl -fsSL https://raw.githubusercontent.com/gaow/misc/master/bash/pixi/init.sh | bash
