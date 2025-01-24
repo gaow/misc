f7ad7a153253e441d80e42828dec4ab5fc4daeb0
gaow <wangow@gmail.com>
2025-01-23 23:00:21 -0500

    Update init.sh

--- bash/pixi/init.sh
+++ bash/pixi/init.sh
@@ -85,7 +85,5 @@ fi
 # Temporary fix to run post-link scripts
 if [[ -d ${HOME}/.pixi/envs/r-base/bin ]]; then
    find ${HOME}/.pixi/envs/r-base/bin -name '*bioconductor-*-post-link.sh' | \
-   while read script; do
-      PREFIX="${HOME}/.pixi/envs/r-base" PATH="${HOME}/.pixi/envs/r-base/bin:${PATH}" bash "$script"
-   done
+   xargs -I % bash -c "PREFIX=${HOME}/.pixi/envs/r-base PATH=${HOME}/.pixi/envs/r-base/bin:${PATH} %"
 fi
